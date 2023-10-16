d902fffc22914d1cc7a0997f49cc7588a846fbd2
xdvoi <148000358+xdvoi@users.noreply.github.com>
2023-10-16 21:45:48 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -77,8 +77,7 @@ async def gen_thumb(videoid):
         font2 = ImageFont.truetype("assets/font2.ttf", 70)
         arial = ImageFont.truetype("assets/font2.ttf", 30)
         name_font = ImageFont.truetype("assets/font.ttf", 30)
-        para = textwrap.wrap(title, width=1)
-        j = 0
+        draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
         draw.text(
             (5, 5), f"", fill="white", font=name_font
         )
